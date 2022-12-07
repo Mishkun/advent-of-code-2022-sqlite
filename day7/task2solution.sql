5c95d83bd7075ba935665a3c408c922b2d5c2dc1
Mishkun <themishkun@yandex-team.ru>
2022-12-07 13:32:51 +0100

    Completed task 2 for day 7

---
+++ day7/task2solution.sql
@@ -0,0 +1,60 @@
+create table inp(x text);
+
+.import input.txt inp
+.mode table
+
+with recursive preparsed as(
+     select
+            case when x = '$ cd ..' then 'dc'
+                 when x like '$ cd %' then 'cd'
+                 when x like 'dir %' then 'dir'
+                 else 'file'
+            end as t,
+            case when x = '$ cd ..' then null
+                 when x like '$ cd %' then substr(x, 6)
+                 when x like 'dir %' then substr(x, 5)
+                 else substr(x, instr(x, ' ') + 1)
+            end as nm,
+            iif(x not like '$%' and x not like 'dir%', cast(substr(x, 1, instr(x, ' ')) as int), null) as bytes,
+            row_number() over (order by (select null)) as n from inp
+            where x <> '$ ls' and  x <> '$ cd /'
+),
+-- parse directory structure, saving current path in pth
+parsed as (
+      select 'root' as t, '/' as nm, null as bytes, json_array('/') as pth, 1 as step
+      union all
+      select
+        preparsed.t,
+        case when preparsed.t = 'dir' or preparsed.t = 'file'
+             then json_insert(pth, '$[#]', preparsed.nm)
+             else preparsed.nm
+        end as nm,
+        preparsed.bytes,
+        case when preparsed.t = 'cd'
+             then json_insert(pth, '$[#]', preparsed.nm)
+             when preparsed.t = 'dc'
+             then json_remove(pth, '$[#-1]')
+             else pth
+        end as pth,
+        step + 1 as step
+        from parsed
+        inner join preparsed
+             on n = step
+),
+only_dirs as (
+       select json_remove(nm, '$[#-1]') as nm, sum(bytes) as bytes from parsed
+       where t = 'file'
+       group by json_remove(nm, '$[#-1]')
+),
+solution as (
+     select json_each.value as dirnm, sum(bytes) as bytes from only_dirs, json_each(only_dirs.nm)
+     group by json_extract(nm, printf('$[%s]', iif(json_each.key > 0,json_each.key - 1, 0))) || json_each.value
+     order by nm
+),
+space_to_free as (
+     select max(bytes) - 40000000 from solution
+)
+select bytes as answer from solution
+where (select * from space_to_free) < bytes
+order by bytes
+limit 1;
